1d5fef11f82c45573bdf37aee930af4813107128
dhershman1 <dustinh17@gmail.com>
2024-04-13 19:01:19 -0400

    Add: New Task

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -60,7 +60,7 @@ const ctrl = useKeyModifier('Control')
             </template>
           </draggable>
           <footer>
-            <button class="text-gray-500">+ Add a Card</button>
+            <NewTask @add="column.tasks.push($event)" />
           </footer>
         </div>
       </template>
