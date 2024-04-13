2fea60034aec55a74cf864cfdfc5f9bc1f57076c
dhershman1 <dustinh17@gmail.com>
2024-04-13 18:26:40 -0400

    Added: Trello task component

---
+++ components/TrelloBoardTask.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Task } from '@/types'
+
+defineProps<{
+  task: Task
+}>()
+</script>
+
+<template>
+  <div
+    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]"
+    :title="task.createdAt.toLocaleDateString()"
+  >
+    <span>{{ task.title }}</span>
+  </div>
+</template>
